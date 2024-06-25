6a354efe330f27c8a94b71a1c5a97b27358eca17
dirasharajbhandari <dirasharajbhandari@gmail.com>
2024-06-25 22:17:15 +0545

    update by Dirasha Rajbhandari on 10:16

---
+++ Untitled1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int firstNumber,secondNumber,Sum;
+     printf("Enter any two numbers:");  
+     scanf("%d %d,&firstNumber &secondNumber");
+     sum=firstNumber+secondNumber;
+     printf("Total:%d,sum");
+    
+    return 0;
+     
+    
+}
