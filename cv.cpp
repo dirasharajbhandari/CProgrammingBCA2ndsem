1e274dd1644c617861ef9c174c78d00ed0d2df3c
dirasharajbhandari <dirasharajbhandari@gmail.com>
2024-06-26 21:06:19 +0545

    Updated by Dirasha Rajbhnadari on June 26 at 9:06 pm

---
+++ cv.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+int main()
+{
+    char section;
+    int roll,age;
+    char name[20],faculty[20],email[20];
+
+    printf("Enter the Name:");
+    printf("\n");
+    scanf("%s",&name);
+    printf("\n");
+    printf("Enter the roll no:");
+    scanf("%d",&roll);
+
+    printf("\n");
+
+    printf("Enter the faculty:");
+
+    printf("\n");
+
+    scanf("%s",&faculty);
+   
+    printf("\n");
+
+    printf("Enter the age:");
+
+    printf("\n");
+    
+    scanf("%d",&age);
+
+    printf("\n");
+
+    printf("Enter the email:");
+
+    printf("\n");
+
+    scanf("%d",&email);
+
+    printf("\n");
+
+    printf("Enter the section:");
+
+    printf("\n");
+
+    scanf(" %c",&section);
+
+    printf("\n");
+
+
+    return 0;
+    
+
+
+
+
+
+}
